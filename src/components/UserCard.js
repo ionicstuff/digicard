483f94bf8180fe5fadac70572711b80cf72a5a59
ionicstuff <vaibhav.nadgonde1980@gmail.com>
2020-07-20 08:17:38 +0530

    digicard changed

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import CompanyName from './CompanyName';
+import phoneNumber, { PhoneNumber } from './PhoneNumber';
 
 export class UserCard extends Component {
   state = {
@@ -8,24 +10,42 @@ export class UserCard extends Component {
 
   //Next step
   nextStep = () => {
-      const { step } = this.state;
-      this.setState({
-          step = step + 1
-      });
+    const { step } = this.state;
+    this.setState({
+      step: step + 1,
+    });
   };
   //Prev step
   nextStep = () => {
     const { step } = this.state;
     this.setState({
-        step = step - 1
+      step: step - 1,
     });
-};
+  };
   //handle change
-  handleChange = input = e =>{
-      this.setState({ [input]: e.target.value});
-  }
+  handleChange = (input) => (e) => {
+    this.setState({ [input]: e.target.value });
+  };
   render() {
-    return <div></div>;
+    const { step, phoneNumber } = this.state;
+    const values = { phoneNumber };
+
+    switch (step) {
+      case 1:
+        return (
+          <PhoneNumber
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return <h1>Company basic details</h1>;
+      case 3:
+        return <h1>Company about us details</h1>;
+      case 4:
+        return <h1>card success</h1>;
+    }
   }
 }
 
